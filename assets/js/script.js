a5bc53e1773ccb8e9d9c1b01226eb341c9b2ac84
Ulysses <This2willpass@aol.com>
2019-10-28 23:32:06 -0700

    HTMl and css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,42 @@ $("#city-search").on("click", function() {
         })
  
   });
+
+
+
+
+
+
+
+  
+//   function initCoords() {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(initialize, locationError);
+//     } else {
+//       showError("Your browser does not support Geolocation!");
+//     }
+//   }
+
+//   function initialize(position) {
+//     var lat = position.coords.latitude;
+//     var lon = position.coords.longitude;
+//     var acc = position.coords.accuracy;
+
+//     // Debugging
+//     console.log(position.coords);
+//     console.log("Accuracy: "+acc+"\nLatitude: "+lat+"\nLongitude: "+lon);
+
+//     // Google Maps API
+//     var myLatlng = new google.maps.LatLng(lat,lon);
+//     var mapOptions = {
+//         center: new google.maps.LatLng(lat, lon),
+//         zoom: 12,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+//     var marker = new google.maps.Marker({
+//         position: myLatlng,
+//         map: map,
+//         title:"Hello World!"
+//     });
+// }
